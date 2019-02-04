5204bebbf0b51dcb64408736c490e8d85c9bcac1
Sebastien <sebastien@migniot.com>
2019-02-05 00:45:25 +0100

    React todolist experiment

---
+++ Todolist2/precache-manifest.e507dd52cac8c9faab46252e4196dd0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2209cf9e3bdbf6ea69d5",
+    "url": "./static/js/main.2209cf9e.chunk.js"
+  },
+  {
+    "revision": "5cb9b55e9859431b45ee",
+    "url": "./static/js/1.5cb9b55e.chunk.js"
+  },
+  {
+    "revision": "2209cf9e3bdbf6ea69d5",
+    "url": "./static/css/main.e8a385b7.chunk.css"
+  },
+  {
+    "revision": "fa3ccaa5975ab89f682091a233223182",
+    "url": "./index.html"
+  }
+];
--- Todolist2/service-worker.js
+++ Todolist2/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.54219331c5bed9de65d6dc08349722aa.js"
+  "./precache-manifest.e507dd52cac8c9faab46252e4196dd0b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
