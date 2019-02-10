14b478d9e294a81f7827817ec5b9bfb57bc44dee
Sebastien <sebastien@migniot.com>
2019-02-10 16:13:49 +0100

    Redux, kinto

---
+++ Todolist4/precache-manifest.a62dce563c84fd8f7d13da59aee4c9f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "d0a10197d85734f16d5a",
+    "url": "./static/js/main.d0a10197.chunk.js"
+  },
+  {
+    "revision": "4f218036f4b68e6a491f",
+    "url": "./static/js/1.4f218036.chunk.js"
+  },
+  {
+    "revision": "d0a10197d85734f16d5a",
+    "url": "./static/css/main.12bddcc9.chunk.css"
+  },
+  {
+    "revision": "c82158f098bdddf4ebc854518a4f14be",
+    "url": "./index.html"
+  }
+];
